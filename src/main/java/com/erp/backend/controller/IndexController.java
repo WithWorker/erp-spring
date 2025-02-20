ca8cf210ada5feb3aab9b86cf340565653c06c6e
kms322 <kms970322@gmail.com>
2025-02-20 09:34:25 +0900

    setting

---
+++ src/main/java/com/erp/backend/controller/IndexController.java
@@ -0,0 +1,16 @@
+package com.erp.backend.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class IndexController {
+    @GetMapping({"","/"})
+    public String home(){
+        return "home";
+    }
+}
--- src/main/java/com/erp/backend/controller/MemberController.java
+++ src/main/java/com/erp/backend/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.erp.backend.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
 @Controller
