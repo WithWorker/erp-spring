6cd2e44e440e63eaf2e77fd74701b7e3e05430f1
kms322 <kms970322@gmail.com>
2025-02-27 17:00:44 +0900

    logout

--- src/main/java/com/erp/backend/config/SecurityConfig.java
+++ src/main/java/com/erp/backend/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.erp.backend.config;
 
 import com.erp.backend.dto.MemberRole;
 import com.erp.backend.filter.AuthenticationFilter;
+import com.erp.backend.filter.CustomLogoutFilter;
 import com.erp.backend.filter.JwtFilter;
 import com.erp.backend.service.MemberDetailsService;
 import com.erp.backend.util.JwtUtil;
@@ -13,10 +14,12 @@ import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -59,7 +62,10 @@ public class SecurityConfig {
                         .anyRequest().permitAll())
 
                 .addFilterBefore(new JwtFilter(jwtUtil), AuthenticationFilter.class)
-                .addFilterAt(authFilter, UsernamePasswordAuthenticationFilter.class);
+                .addFilterAt(authFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterAt(new CustomLogoutFilter(), LogoutFilter.class)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
         return http.build();
     }
 
