58472271a05dcbc3a4b827c6e3793c33c0cb1b78
KangKyunghun <rudgns1101@naver.com>
2025-03-26 14:35:16 +0900

    controller 이니셜 추가

--- src/main/java/com/erp/backend/controller/MessengerController.java
+++ src/main/java/com/erp/backend/controller/MessengerController.java
@@ -28,7 +28,7 @@ import java.util.Map;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/messenger")
+@RequestMapping("/kkh/messenger")
 public class MessengerController {
 
     @Autowired
